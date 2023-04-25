e8ef55c31d7b4258f4c517e6caa61a0884f98adb
Araghi <aaaldana50@gmail.com>
2023-04-24 22:28:04 -0500

    añadido estructura de proyecto

--- bd.py
+++
@@ -1,8 +0,0 @@
-import pymysql
-
-def obtener_conexion():
-    return pymysql.connect(host='127.0.0.1',
-                                port=3306,
-                                user='testuser',
-                                password='123456',
-                                db='proyecto_calidad')
