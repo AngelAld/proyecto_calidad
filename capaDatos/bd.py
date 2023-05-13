c3962fe64c07d52b0f818baa36a1229e31e0d2b7
Araghi <aaaldana50@gmail.com>
2023-05-12 23:39:23 -0500

    test

--- capaDatos/bd.py
+++ capaDatos/bd.py
@@ -2,9 +2,17 @@
 import psycopg2
 
 
+# def obtener_conexion():
+#     return psycopg2.connect(host='127.0.0.1',
+#                             port=5432,
+#                             user='postgres',
+#                             password='USAT2023',
+#                             database='proyecto_calidad')
+
+# si no tienen postgres instalado pueden usar esta bd online
 def obtener_conexion():
-    return psycopg2.connect(host='127.0.0.1',
+    return psycopg2.connect(host='jelani.db.elephantsql.com',
                             port=5432,
-                            user='postgres',
-                            password='USAT2023',
-                            database='proyecto_calidad')
+                            user='nzsbwaov',
+                            password='KGMLF1yVbv70Y7GqkfPRPVNL0C0GWQ2O',
+                            database='nzsbwaov')
