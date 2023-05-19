fa242464b432a9a38c2a861b5aab7035aaffad34
Araghi <aaaldana50@gmail.com>
2023-05-19 12:49:51 -0500

    Busqueda

--- capaPresentacion/static/js/sidebar.js
+++ capaPresentacion/static/js/sidebar.js
@@ -5,17 +5,17 @@ toggleButton.onclick = function () {
     el.classList.toggle("toggled");
 };
 
+// Esto Causa problemas
+// sidebar.addEventListener("mouseover", function () {
+//     if (el.classList.contains("toggled")) {
+//         el.classList.toggle("toggler");
+//         el.classList.toggle("toggled");
+//     }
+// });
 
-sidebar.addEventListener("mouseover", function () {
-    if (el.classList.contains("toggled")) {
-        el.classList.toggle("toggler");
-        el.classList.toggle("toggled");
-    }
-});
-
-sidebar.addEventListener("mouseout", function () {
-    if (el.classList.contains("toggler")) {
-        el.classList.toggle("toggler");
-        el.classList.toggle("toggled");
-    }
-});
+// sidebar.addEventListener("mouseout", function () {
+//     if (el.classList.contains("toggler")) {
+//         el.classList.toggle("toggler");
+//         el.classList.toggle("toggled");
+//     }
+// });
