b4603ac29b30ef716cbb8faefc8f239793f10ea3
IvanGC20 <70083467@usat.pe>
2023-06-12 09:17:05 -0500

    agregué estudiante

--- capaNegocio/controlador_Estudiante.py
+++ capaNegocio/controlador_Estudiante.py
@@ -1,63 +1,69 @@
 from capaDatos.bd import obtener_conexion
 
-def listar_docentes():
-    conexion = obtener_conexion()
-    docentes = []
-    # with conexion.cursor() as cursor:
-    #     cursor.execute("select * from fn_listar_docente_apoyo()")
-    #     docentes = cursor.fetchall()
-    conexion.close()
-    return docentes
 
-def agregar_docente(nombre, correo, id_titulo, id_escuela_profesional, id_usuario, estado):
+def getAll():
+    conexion = obtener_conexion()
+    semestres = []
+    # with conexion.cursor() as cursor:
+    #     cursor.execute("select * from fn_read_semestres()")
+    #     semestres = cursor.fetchall()
+    conexion.close()
+    return semestres
+
+
+def insert(nombre, fecha_inicio, fecha_fin, estado):
     conexion = obtener_conexion()
     msg = []
     # with conexion.cursor() as cursor:
     #     cursor.execute(
-    #         "SELECT fn_agregar_docente_apoyo(%s, %s, %s, %s, %s, %s)",
-    #         (nombre, correo, id_titulo, id_escuela_profesional, id_usuario, estado),
+    #         "SELECT fn_create_semestre(%s, %s, %s, %s)",
+    #         (nombre, fecha_inicio, fecha_fin, estado),
     #     )
     #     msg = cursor.fetchone()
     # conexion.commit()
     conexion.close()
     return msg[0] if msg is not None else None
 
-def eliminar_docente(id):
+
+def delete(id):
     conexion = obtener_conexion()
     msg = []
     # with conexion.cursor() as cursor:
-    #     cursor.execute("SELECT fn_delete_docente(%s)", (id,))
+    #     cursor.execute("SELECT fn_delete_semestre(%s)", (id,))
     #     msg = cursor.fetchone()
     # conexion.commit()
     conexion.close()
     return msg[0] if msg is not None else None
 
-def buscar_docenteID(id):
+
+def getById(id):
     conexion = obtener_conexion()
-    docente = None
+    semestre = None
     # with conexion.cursor() as cursor:
     #     cursor.execute(
-    #         "SELECT da.id_docente_apoyo, da.nombre, da.correo, ti.nombre, ep.nombre, us.usuario, da.estado FROM DOCENTE_APOYO da INNER JOIN titulo ti ON ti.id_titulo = da.id_titulo INNER JOIN escuela_profesional ep ON ep.id_escuela_profesional = da.id_escuela_profesional INNER JOIN usuario us ON us.id_usuario = us.id_usuario WHERE da.id_docente_apoyo=%s",
+    #         "SELECT id_semestre, nombre, fecha_inicio, fecha_fin, estado FROM SEMESTRE_ACADEMICO WHERE id_semestre=%s",
     #         (id,),
     #     )
-    #     docente = cursor.fetchone()
+    #     semestre = cursor.fetchone()
     conexion.close()
-    return docente
+    return semestre
 
-def actualizar_docente(id, nombre, correo, id_titulo, id_escuela_profesional, id_usuario, estado):
+
+def update(id, nombre, fecha_inicio, fecha_fin, estado):
     conexion = obtener_conexion()
     msg = []
     # with conexion.cursor() as cursor:
     #     cursor.execute(
-    #         "SELECT fn_update_semestre(%s, %s, %s, %s, %s, %s, %s)",
-    #         (id, nombre, correo, id_titulo, id_escuela_profesional, id_usuario, estado),
+    #         "SELECT fn_update_semestre(%s, %s, %s, %s, %s)",
+    #         (id, nombre, fecha_inicio, fecha_fin, estado),
     #     )
     #     msg = cursor.fetchone()
     # conexion.commit()
     conexion.close()
     return msg[0] if msg is not None else None
 
-def dar_baja_docente(id, estado):
+
+def update_estado(id, estado):
     conexion = obtener_conexion()
     msg = []
     # new_estado = ""
