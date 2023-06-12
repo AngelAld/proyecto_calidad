d0caafd07138da893e1c241ec4e26c58fac33854
Crystian Culqui <crys100culqui@gmail.com>
2023-06-12 09:23:43 -0500

    informe inicial empresa

---
+++ capaNegocio/controlador_informe_inicial_em.py
@@ -0,0 +1,10 @@
+from capaDatos.bd import obtener_conexion
+
+def getAll():
+     conexion = obtener_conexion()
+     informe_inicial_em = []
+    # with conexion.cursor() as cursor:
+    #     cursor.execute("select * from fn_read_semestres()")
+    #     informe_inicial_es = cursor.fetchall()
+     conexion.close()
+     return informe_inicial_em
