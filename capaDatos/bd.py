e7e5aaa592e1a463758957418cc01ecc0ad6a778
Eliana Barturen <bartureneliana1@gmail.com>
2023-06-18 08:51:35 -0500

    facultadv2

--- capaDatos/bd.py
+++ capaDatos/bd.py
@@ -1,19 +1,7 @@
 # import pymysql
 import psycopg2
 
-def obtener_conexion():
-      return psycopg2.connect(host='jelani.db.elephantsql.com',
-                              port=5432,
-                              user='nzsbwaov',
-                              password='KGMLF1yVbv70Y7GqkfPRPVNL0C0GWQ2O',
-                              database='nzsbwaov')
 
-# def obtener_conexion():
-#     return psycopg2.connect(host='127.0.0.1',
-#                             port=5432,
-#                             user='postgres',
-#                             password='123456789',
-#                             database='bd_calidad')
 #def obtener_conexion():
 #    return psycopg2.connect(host='127.0.0.1',
 #                            port=5432,
@@ -23,10 +11,9 @@ def obtener_conexion():
 
 # si no tienen postgres instalado pueden usar esta bd online
 
-# def obtener_conexion():
-#     return psycopg2.connect(host='jelani.db.elephantsql.com',
-#                             port=5432,
-#                             user='nzsbwaov',
-#                             password='KGMLF1yVbv70Y7GqkfPRPVNL0C0GWQ2O',
-#                             database='nzsbwaov')
-
+def obtener_conexion():
+      return psycopg2.connect(host='jelani.db.elephantsql.com',
+                              port=5432,
+                              user='nzsbwaov',
+                              password='KGMLF1yVbv70Y7GqkfPRPVNL0C0GWQ2O',
+                              database='nzsbwaov')
