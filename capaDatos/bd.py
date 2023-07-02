57c57d7087e47d3e0870f63e5a1cbfe80244fb0e
Alexander091196 <91032455+Alexander091196@users.noreply.github.com>
2023-07-02 14:18:41 -0500

    reporte-lineaDesarrollo

--- capaDatos/bd.py
+++ capaDatos/bd.py
@@ -2,18 +2,18 @@
 import psycopg2
 
 
-def obtener_conexion():
-    return psycopg2.connect(host='angelar-3301.postgres.pythonanywhere-services.com',
-                            port=13301,
-                            user='super',
-                            password='PROYECTOCALIDAD',
-                            database='db_calidad')
-
-# si no tienen postgres instalado pueden usar esta bd online
-
 # def obtener_conexion():
-#       return psycopg2.connect(host='jelani.db.elephantsql.com',
-#                               port=5432,
-#                               user='nzsbwaov',
-#                               password='KGMLF1yVbv70Y7GqkfPRPVNL0C0GWQ2O',
-#                               database='nzsbwaov')
+#     return psycopg2.connect(host='angelar-3301.postgres.pythonanywhere-services.com',
+#                             port=13301,
+#                             user='super',
+#                             password='PROYECTOCALIDAD',
+#                             database='db_calidad')
+
+
+
+def obtener_conexion():
+      return psycopg2.connect(host='jelani.db.elephantsql.com',
+                              port=5432,
+                              user='nzsbwaov',
+                              password='KGMLF1yVbv70Y7GqkfPRPVNL0C0GWQ2O',
+                              database='nzsbwaov')
