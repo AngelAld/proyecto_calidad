3559cb273cae62f1c978e7f7ab1b053e2ee46b81
Araghi <aaaldana50@gmail.com>
2023-07-02 18:48:56 -0500

    bd

--- capaDatos/bd.py
+++ capaDatos/bd.py
@@ -2,17 +2,17 @@
 import psycopg2
 
 
-# def obtener_conexion():
-#     return psycopg2.connect(host='angelar-3301.postgres.pythonanywhere-services.com',
-#                             port=13301,
-#                             user='super',
-#                             password='PROYECTOCALIDAD',
-#                             database='db_calidad')
-
-
 def obtener_conexion():
-      return psycopg2.connect(host='jelani.db.elephantsql.com',
-                              port=5432,
-                              user='nzsbwaov',
-                              password='KGMLF1yVbv70Y7GqkfPRPVNL0C0GWQ2O',
-                              database='nzsbwaov')
+    return psycopg2.connect(host='angelar-3301.postgres.pythonanywhere-services.com',
+                            port=13301,
+                            user='super',
+                            password='PROYECTOCALIDAD',
+                            database='db_calidad')
+
+
+# def obtener_conexion():
+#       return psycopg2.connect(host='jelani.db.elephantsql.com',
+#                               port=5432,
+#                               user='nzsbwaov',
+#                               password='KGMLF1yVbv70Y7GqkfPRPVNL0C0GWQ2O',
+#                               database='nzsbwaov')
