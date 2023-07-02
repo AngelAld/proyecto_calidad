bd383e7c9bfeb3401ccd5e05066e5cd8e91b906b
Araghi <aaaldana50@gmail.com>
2023-07-02 13:21:02 -0500

    cambios bd

--- capaDatos/bd.py
+++ capaDatos/bd.py
@@ -2,18 +2,18 @@
 import psycopg2
 
 
-# def obtener_conexion():
-#     return psycopg2.connect(host='127.0.0.1',
-#                             port=5432,
-#                             user='postgres',
-#                             password='123456789',
-#                             database='bd_calidad')
+def obtener_conexion():
+    return psycopg2.connect(host='angelar-3301.postgres.pythonanywhere-services.com',
+                            port=13301,
+                            user='super',
+                            password='PROYECTOCALIDAD',
+                            database='db_calidad')
 
 # si no tienen postgres instalado pueden usar esta bd online
 
-def obtener_conexion():
-      return psycopg2.connect(host='jelani.db.elephantsql.com',
-                              port=5432,
-                              user='nzsbwaov',
-                              password='KGMLF1yVbv70Y7GqkfPRPVNL0C0GWQ2O',
-                              database='nzsbwaov')
+# def obtener_conexion():
+#       return psycopg2.connect(host='jelani.db.elephantsql.com',
+#                               port=5432,
+#                               user='nzsbwaov',
+#                               password='KGMLF1yVbv70Y7GqkfPRPVNL0C0GWQ2O',
+#                               database='nzsbwaov')
