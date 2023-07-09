640846d2934df7d8039649ecf845cc7ddd32035f
unknown <decabrejos@gmail.com>
2023-07-08 19:59:35 -0500

    haciendo funcionar ficha de desempeno

--- capaPresentacion/ficha_desempeno/ficha_desempeno.py
+++ capaPresentacion/ficha_desempeno/ficha_desempeno.py
@@ -9,11 +9,11 @@ def ficha_desempeno():
         return redirect(url_for("inicio.inicio"))
     else:
         try:
-            ficha_desempeno = c_ficha_desempeno.getAll()
-            if isinstance(ficha_desempeno,list):
-                return render_template("ficha_desempeno.html", ficha_desempeno=ficha_desempeno)
+            ficha_desempenos = c_ficha_desempeno.listar_fichas_desempeno()
+            if isinstance(ficha_desempenos,list):
+                return render_template("ficha_desempeno.html", ficha_desempenos=ficha_desempenos)
             else:
-                print(str(ficha_desempeno))
+                print(str(ficha_desempenos))
                 return redirect(url_for("inicio.inicio"))
         except Exception as e:
             print(f"Error al obtener fichas de desempeño: {e}")
@@ -25,3 +25,37 @@ def agregar_ficha_desempeno():
         return redirect(url_for("inicio.inicio"))
     else:
         return render_template("mantenimiento_ficha_desempeno.html")
+    
+@ficha_desempeno_bp.route("/actualizar_estado_ficha_desempeno", methods=["POST"])
+def actualizar_estado():
+    if "rol" not in session or session["rol"] != "Docente de Apoyo":
+        return redirect(url_for("inicio.inicio"))
+    else:
+        id_ficha = request.form["id"]
+        estado = request.form["estado"]
+        mensaje = c_ficha_desempeno.dar_baja_ficha_desempeno(id_ficha, estado)
+
+        if mensaje == "Operación realizada con éxito":
+            flash("Ficha de Desempeño Actualizada con Éxito", "success")
+        else:
+            flash(str(mensaje), "error")
+        return redirect(url_for("ficha_desempeno.ficha_desempeno"))
+
+@ficha_desempeno_bp.route("/estudiante/editar_ficha_desempeno/<int:id>")
+def editar_ficha_desempeno(id):
+    (
+        estudiante,
+        datos_cppp,
+        datos_practica,
+        resultados_aprendizaje,
+        ficha_desempeno,
+    ) = c_ficha_desempeno.consultar_ficha_desempeno(id)
+
+    return render_template(
+        "frm_editar_ficha_desempeno.html",
+        estudiante=estudiante,
+        datos_cppp=datos_cppp,
+        datos_practica=datos_practica,
+        resultados_aprendizaje=resultados_aprendizaje,
+        ficha_desempeno=ficha_desempeno,
+    )
