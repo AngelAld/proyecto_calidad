839f14aa50feb4c2476b2d28f0bbf9596e973d01
unknown <decabrejos@gmail.com>
2023-07-12 02:13:20 -0500

    haciendo funcionar informe final estudiante

--- capaNegocio/controlador_informe_final_es.py
+++ capaNegocio/controlador_informe_final_es.py
@@ -42,4 +42,72 @@ def dar_baja_informe_final_estudiante(p_id_informe_final_es, p_estado):
         return "Operación realizada con éxito"
     except Exception as e:
         return f"Error al cambiar el estado del informe final del estudiante: {str(e)}"
+    
+    
+def consultar_informe_finales_estudiante(id_informe_final_es):
+    try:
+        conexion = obtener_conexion()
+
+        with conexion.cursor() as cursor:
+            # Obtener nombre y código universitario del estudiante
+            cursor.execute("""
+                SELECT e.nombre AS nombre_estudiante, ep.nombre AS nombre_escuela_profesional
+                FROM ESTUDIANTE e
+                JOIN PRACTICA p ON e.id_estudiante = p.id_estudiante
+                JOIN DETALLE_PRACTICA dp ON p.id_practica = dp.id_practica
+                JOIN INFORME_FINAL_ES ife ON dp.id_detalle_practica = ife.id_detalle_practica
+                JOIN PLAN_ESTUDIO pe ON e.id_plan_estudio = pe.id_plan_estudio
+                JOIN ESCUELA_PROFESIONAL ep ON pe.id_escuela_profesional = ep.id_escuela_profesional
+                WHERE ife.id_informe_final_es = %s;
+            """, (id_informe_final_es,))
+            estudiante = cursor.fetchone()
+
+            # Obtener información del centro de prácticas, jefe inmediato, y cargo del jefe inmediato
+            cursor.execute("""
+                SELECT cp.razon_social, u.ciudad, cp.rubro, cp.alias, ji.nombre AS representante_legal
+                FROM CENTRO_PRACTICAS cp
+                JOIN UBICACION u ON cp.id_ubicacion = u.id_ubicacion
+                JOIN PRACTICA p ON cp.id_centro_practicas = p.id_estudiante
+                JOIN DETALLE_PRACTICA dp ON p.id_practica = dp.id_practica
+                JOIN INFORME_FINAL_ES ife ON dp.id_detalle_practica = ife.id_detalle_practica
+                JOIN JEFE_INMEDIATO ji ON cp.id_centro_practicas = ji.id_centro_practicas
+                WHERE ife.id_informe_final_es = %s;
+            """, (id_informe_final_es,))
+            datos_cppp = cursor.fetchone()
+
+            # Obtener información del detalle de práctica
+            cursor.execute("""
+                SELECT sa.nombre, dp.fecha_inicio, dp.fecha_fin
+                FROM DETALLE_PRACTICA dp
+                JOIN SEMESTRE_ACADEMICO sa ON dp.id_semestre_academico = sa.id_semestre
+                JOIN INFORME_INICIAL_ES iie ON dp.id_detalle_practica = iie.id_detalle_practica
+                WHERE iie.id_informe_inicial_es = %s
+            """, (id_informe_final_es,))
+            datos_practica = cursor.fetchone()
+
+            # Obtener todos los registros de la tabla objetivo asociados al informe inicial
+            cursor.execute("""
+                SELECT *
+                FROM OBJETIVO
+                WHERE id_informe_inicial_es = %s
+            """, (id_informe_final_es,))
+            objetivos = cursor.fetchall()
+
+            # Obtener todos los registros de la tabla plan_trabajo asociados al informe inicial
+            cursor.execute("""
+                SELECT *
+                FROM PLAN_TRABAJO
+                WHERE id_informe_inicial_es = %s
+            """, (id_informe_final_es,))
+            plan_trabajo = cursor.fetchall()
+
+            cursor.execute("SELECT * FROM informe_inicial_es WHERE id_informe_inicial_es = %s", (id_informe_final_es,))
+            informe = cursor.fetchone()
+        conexion.close()
+
+        return estudiante, datos_cppp, datos_practica, objetivos, plan_trabajo, informe
+
+    except Exception as e:
+        print(f"Error al consultar informe inicial: {str(e)}")
+
 
--- capaPresentacion/informe_final_es/informe_final_es.py
+++ capaPresentacion/informe_final_es/informe_final_es.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, flash, session, url_for
 from capaNegocio import controlador_informe_final_es as c_informe_final_es
+import os
+from datetime import datetime, date
+from werkzeug.utils import secure_filename
 
 informe_final_es_bp = Blueprint("informe_final_es", __name__, template_folder="templates")
 
@@ -32,6 +35,93 @@ def actualizar_estado():
         else:
             flash(str(mensaje), "error")
         return redirect(url_for("informe_final_es.informe_final_es"))
+
+@informe_final_es_bp.route("/editar_informe_final_es/<int:id>")
+def frm_editar_informe_final_es(id):
+    return redirect(url_for("inicio.inicio"))
+
+
+@informe_final_es_bp.route("/estudiante/editar_informe_final_es/<int:id>")
+def editar_informe_final_es(id):
+    (
+        estudiante,
+        datos_cppp,
+        datos_practica,
+        objetivos,
+        plan_trabajo,
+        informe,
+    ) = c_informe_final_es.consultar_informe_finales_estudiante(id)
+
+    return render_template(
+        "frm_editar_informe_final_es.html",
+        estudiante=estudiante,
+        datos_cppp=datos_cppp,
+        datos_practica=datos_practica,
+        objetivos=objetivos,
+        plan_trabajo=plan_trabajo,
+        informe=informe,
+    )
+@informe_final_es_bp.route("/actualizar_informe_final_es", methods=["POST"])
+def actualizar_informe_final_es():
+    if "rol" not in session or session["rol"] != "Docente de Apoyo":
+        return redirect(url_for("inicio.inicio"))
+    else:
+        id_informe_final_es = request.form['id_informe_final_es']
+        fecha_inicio = request.form['fecha_inicio']
+        fecha_fin = request.form['fecha_fin']
+        total_horas = request.form["totalHoras"]
+        # Subir archivos de firma
+        firma_es = ""
+        firma_jefe = ""
+
+        if 'firma_estudiante' in request.files:
+            f = request.files['firma_estudiante']
+            if f.filename != '':
+                filename = secure_filename(f.filename)
+                f.save(os.path.join(UPLOAD_FOLDER, filename))
+                firma_es = os.path.join(UPLOAD_FOLDER, filename)
+
+        if 'firma_jefe' in request.files:
+            f = request.files['firma_jefe']
+            if f.filename != '':
+                filename = secure_filename(f.filename)
+                f.save(os.path.join(UPLOAD_FOLDER, filename))
+                firma_jefe = os.path.join(UPLOAD_FOLDER, filename)
+
+        objetivos = request.form.getlist('objetivo[]')
+
+        # Agregar datos de planes de trabajo a una lista
+        plan_trabajo = []
+        n_semanas = request.form.getlist('n_semana[]')
+        fs_inicio = request.form.getlist('fecha_in[]')
+        fs_fin = request.form.getlist('fecha_fin[]')
+        actividades = request.form.getlist('actividad[]')
+        horas = request.form.getlist('horas[]')
+
+        for i in range(len(n_semanas)):
+            plan_trabajo.append({
+                "n_semana": i + 1,
+                "fecha_inicio": fs_inicio[i],
+                "fecha_fin": fs_fin[i],
+                "actividad": actividades[i],
+                "horas": horas[i]
+            })
+
+        mensaje = c_informe_final_es.actualizar_informe_final(fecha_fin=fecha_fin, fecha_inicio=fecha_inicio,
+                                                              id_informe_final_es=id_informe_final_es,
+                                                              firma_es=firma_es, firma_jefe=firma_jefe,
+                                                              descripciones=objetivos, plan_trabajo=plan_trabajo,
+                                                              totalHoras=total_horas)
+        print(mensaje)
+        if mensaje == "Operacion realizada con éxito":
+            flash("Informe Final Actualizado con Éxito", "success")
+            url = "/estudiante/informes_finales"
+        else:
+            print('entramos aqui')
+            flash(mensaje, "error")
+            url = "/estudiante/editar_informe_final/" + id_informe_final_es
+        return redirect(url)
+
 
     
 
